3aab426d03565232f32690eb687bf9f8c8f4b0f4
peterhchen <peter.hp.chen@gmail.com>
2019-05-28 16:06:56 -0700

    Template Shortcut

---
+++ 15_RenderTemplate/RenderTemplate15/music/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render
+from .models import Album
+
+def index (request):
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums }
+    #return HttpResponse(template.render(context, request))
+    return render (request, 'music/index.html', context)
+
+def detail(reques, album_id):
+    return HttpResponse ("<h2>details for Album id: " + str (album_id) + "</h2>")
