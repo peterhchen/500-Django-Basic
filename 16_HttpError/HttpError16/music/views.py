3fa4b6965ea27f607f1274a740be68c98b0ef165
peterhchen <peter.hp.chen@gmail.com>
2019-05-29 10:09:36 -0700

    Http Error

---
+++ 16_HttpError/HttpError16/music/views.py
@@ -0,0 +1,20 @@
+from django.http import Http404
+#from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render
+from .models import Album
+
+def index (request):
+    all_albums = Album.objects.all()
+    #context = {'all_albums': all_albums }
+    #return HttpResponse(template.render(context, request))
+    return render (request, 'music/index.html', {'all_albums': all_albums })
+
+def detail(request, album_id):
+    # we want to check the database and check the album is there or not.
+    #return HttpResponse ("<h2>details for Album id: " + str (album_id) + "</h2>")
+    try:
+        album = Album.objects.get (pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404 ("Album does not exist")
+    return render (request, 'music/detail.html', {'album': album })
