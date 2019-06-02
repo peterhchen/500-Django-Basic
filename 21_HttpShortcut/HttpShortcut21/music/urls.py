afb3f580168b0bf79f721b67550653a83876272f
peterhchen <peter.hp.chen@gmail.com>
2019-06-02 13:13:02 -0700

    Add files via upload

--- 21_HttpShortcut/HttpShortcut21/music/urls.py
+++ 21_HttpShortcut/HttpShortcut21/music/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
